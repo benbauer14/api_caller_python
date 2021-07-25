72653b411de55a16657d9767ca1f3b8225948460
Ben Bauer <ben.bauer1@gmail.com>
2021-07-25 17:44:30 -0500

    evaluates whether the time is after the sunset and before the sunrise and then evaluates whether the ISS is overhead.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import json
 from datetime import datetime
 
 MY_LAT = 44.876786683641164 
@@ -10,26 +9,38 @@ parameters ={
     "lng": MY_LONG,
     "formatted": 0
 }
+#ISS location
 res = requests.get(url="http://api.open-notify.org/iss-now.json")
 res.raise_for_status()
+
+iss_lat = float(res.json()["iss_position"]["latitude"])
+iss_long = float(res.json()["iss_position"]["longitude"])
+
+def isOverHead():
+    #This program assumes the ISS is overhead if the ISS lat and long are +/-5 from my location
+    if(datetime.now().hour < sunrise or datetime.now().hour > sunset):
+        if(abs(iss_lat - MY_LAT) < 5 and  abs(iss_long - MY_LONG)):
+            return True
+        else:
+            return False
+    else:
+        return "It is daytime so it is too light to see the ISS"
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+#find hour of sunrise and sunset
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0]) - 5
 sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-print(int(sunrise)-5)
-
 if int(sunset) <= 4:
-    print(int(sunset) + 19)
+    sunset = (int(sunset) + 19)
 else:  
-    print(int(sunset) - 5)
+    sunset = (int(sunset) - 5)
 
 time_now = datetime.now().hour
 
-print(time_now)
+print(isOverHead())
 
 
 
-print(res.json()["iss_position"])
